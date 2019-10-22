383d285a14445ba0ba33e570025718d64a1cfd1a
Manuel Rodriguez <mrodriguez@livevox.com>
2019-10-22 15:29:05 -0400

    Demo check in
    - Fixed controller to update and create Customers

--- complete/src/main/java/com/livevox/Customer.java
+++ complete/src/main/java/com/livevox/Customer.java
@@ -11,8 +11,32 @@ public class Customer {
     private String firstName;
     @JsonProperty
     private String lastName;
+    
+    public long getId() {
+		return id;
+	}
 
-    public Customer(long id, String firstName, String lastName) {
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Customer(long id, String firstName, String lastName) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
